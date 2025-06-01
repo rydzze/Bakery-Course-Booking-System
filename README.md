146814d80f0b7fc6bf3ab1120ed67b246344f186
Rydzze <rydzze@gmail.com>
2025-06-01 09:43:09 +0800

    Revert "upload BakeStore"
    This reverts commit d8afc7e56307b3b4be69466ab085caf44c35c665.

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# 👨🏻‍🍳 BakeStore: Bakery Shopping System using Semantic Web
+# 🛒 Bakery Course Booking System using Semantic Web
 
-Project for Semantic Web Technology
+## How To Run
+
+1. Install `virtualenv`:
+
+``` 
+$ pip install virtualenv
+```
+
+2. Open a terminal in the project root directory and run:
+
+```
+$ virtualenv venv
+```
+
+3. Then run the command:
+
+```
+$ .\venv\Scripts\activate
+```
+
+4. Then install the dependencies:
+
+```
+$ (venv) pip install -r requirements.txt
+```
+
+5. Finally start the web server:
+
+```
+$ (venv) python run.py
+```
---
+++ app/models.py
@@ -0,0 +1,28 @@
+from flask_login import UserMixin
+from app import db, login
+from datetime import datetime
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    
+    registerCourses = db.relationship('RegisterCourse', backref=db.backref('person', lazy=True ))
+
+    def __repr__(self):
+        return f'{self.username}'
+
+class RegisterCourse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String, nullable=False)
+    quantity = db.Column(db.Integer,nullable=False)
+    date_registered = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'{self.person}'
